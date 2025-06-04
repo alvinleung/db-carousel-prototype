d45e3375728cfc4420dfba40aec8af07366f301b
alvin leung <alvinleung2009@gmail.com>
2025-06-03 21:56:14 -0700

    add example

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -85,7 +85,8 @@ export default function Home() {
         further exploration. 
       </p>
       <AssetCarousel
-        title={"This is a faade carousel"}
+        title={"This carousel uses fade transition"}
+        tag={"demo"}
         safeAreaWidth={906}
         safeAreaHeight={700}
       >
@@ -103,13 +104,64 @@ export default function Home() {
           src={"/asset-content-3.jpg"}
           caption={"Does it really tell me anything new here?"}
         />
+      </AssetCarousel>
+      <p className="max-w-[calc(530px+32px)] mx-auto my-8 px-4">
+        When users initially encounter a technical tool, they can often be
+        overwhelmed by complexity. In Adaline’s early experiences, newcomers
+        encountered a tangle of functionality: redundant UI elements, ambiguous
+        icons, and controls layered with little regard for timing or task
+        relevance. The result was a steep learnability curve that discouraged
+        further exploration. 
+      </p>
+      <AssetCarousel
+        tag={"Demo"}
+        title={"This carousel uses alignment center-column"}
+        titleBarAlignment="center-column"
+        transition="scroll"
+      >
         <AssetCarousel.item
-          src={"/asset-content-3.jpg"}
-          caption={"Does anyone really know what these icons do?"}
+          src={"/asset-content-1.jpg"}
+          caption={
+            "Why are there so many buttons... I just want Anthropic to run my prompt..."
+          }
         />
         <AssetCarousel.item
-          src={"/asset-content-zoomed.jpg"}
-          caption={"This is a zoom test"}
+          src={"/asset-content-2.jpg"}
+          caption={"Wait... they are purely repeat of each other?"}
+        />
+        <AssetCarousel.item
+          src={"/asset-content-3.jpg"}
+          caption={"Does it really tell me anything new here?"}
+        />
+      </AssetCarousel>
+
+      <p className="max-w-[calc(530px+32px)] mx-auto my-8 px-4">
+        When users initially encounter a technical tool, they can often be
+        overwhelmed by complexity. In Adaline’s early experiences, newcomers
+        encountered a tangle of functionality: redundant UI elements, ambiguous
+        icons, and controls layered with little regard for timing or task
+        relevance. The result was a steep learnability curve that discouraged
+        further exploration. 
+      </p>
+      <AssetCarousel
+        tag={"Demo"}
+        title={"This carousel uses safe-area alignment"}
+        titleBarAlignment="safe-area"
+        transition="scroll"
+      >
+        <AssetCarousel.item
+          src={"/asset-content-1.jpg"}
+          caption={
+            "Why are there so many buttons... I just want Anthropic to run my prompt..."
+          }
+        />
+        <AssetCarousel.item
+          src={"/asset-content-2.jpg"}
+          caption={"Wait... they are purely repeat of each other?"}
+        />
+        <AssetCarousel.item
+          src={"/asset-content-3.jpg"}
+          caption={"Does it really tell me anything new here?"}
         />
       </AssetCarousel>
     </main>
